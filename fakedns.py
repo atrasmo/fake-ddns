55390ee1aa4eba6f8ceb7d5f8f6f5b53a57afcaa
Alessandro <atrasmo@users.noreply.github.com>
2017-12-15 01:10:00 +0100

    Create fakedns.py

---
+++ fakedns.py
@@ -0,0 +1,12 @@
+import telepot
+from requests import get
+
+##This is the TOKEN for bot 
+TOKEN = '372667382:AAEyXVmZBgakjpsajdiik90hvJQZSYYJ'
+##This is the channel name
+chat_id ='@<channel-name>'
+
+bot = telepot.Bot(TOKEN)
+
+ip = get('https://api.ipify.org').text
+bot.sendMessage(chat_id, "L'indirizzo IP ora è: {}".format(ip))
